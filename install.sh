f2b4751535572514c188315642b38584d0bac75e
Logan Hennessy <loganjhennessy@gmail.com>
2018-12-28 12:55:13 -0800

    Create an initial install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create the Git directory if one does not exist
+[ -d ${HOME}/Git ] || mkdir ${HOME}/Git
+echo "export WORKY_HOME=${HOME}/Git/" >> ${HOME}/.bash_profile
+
+# Create the worky directory for scratch-spaces
+[ -d ${HOME}/Worky ] || mkdir ${HOME}/Worky
